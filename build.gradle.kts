688f5893d3ca73501620a46aeb84a2f10963b3d6
akarahdev <endisticbutitsforgit@gmail.com>
2025-06-20 09:45:05 -0400

    build: initialize repository

---
+++ build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    id("fabric-loom") version "1.10-SNAPSHOT"
+    id("maven-publish")
+}
+
+version = project.property("mod_version")!!
+group = project.property("maven_group")!!
+
+base {
+    archivesName.set(project.property("archives_base_name") as String)
+}
+
+repositories {
+    // Add repositories to retrieve artifacts from here.
+    // Use only when depending on other mods.
+}
+
+dependencies {
+    val minecraftVersion = project.property("minecraft_version")!!
+    val loaderVersion = project.property("loader_version")!!
+    val fabricVersion = project.property("fabric_version")!!
+
+    minecraft("com.mojang:minecraft:$minecraftVersion")
+    mappings(loom.officialMojangMappings())
+    modImplementation("net.fabricmc:fabric-loader:$loaderVersion")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:$fabricVersion")
+}
+
+tasks.processResources {
+    val minecraftVersion = project.property("minecraft_version")!!
+    val loaderVersion = project.property("loader_version")!!
+
+    inputs.property("version", version)
+    inputs.property("minecraft_version", minecraftVersion)
+    inputs.property("loader_version", loaderVersion)
+    filteringCharset = "UTF-8"
+
+    filesMatching("fabric.mod.json") {
+        expand(
+                mapOf(
+                        "version" to version,
+                        "minecraft_version" to minecraftVersion,
+                        "loader_version" to loaderVersion
+                )
+        )
+    }
+}
+
+val targetJavaVersion = 21
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+        options.release.set(targetJavaVersion)
+    }
+}
+
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+    withSourcesJar()
+}
+
+tasks.jar {
+    from("LICENSE") {
+        rename { "${it}_${project.property("archives_base_name")}" }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = project.property("archives_base_name") as String
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        // Add publishing repositories here
+    }
+}
---
+++ src/main/java/dev/akarah/cdata/Main.java
@@ -0,0 +1,10 @@
+package dev.akarah.cdata;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello world!");
+    }
+}
