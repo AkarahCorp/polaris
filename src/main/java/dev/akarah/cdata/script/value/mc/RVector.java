27be7be125b75d1f645a64303652add4382d36f2
akarahdev <endisticbutitsforgit@gmail.com>
2025-07-17 20:17:52 -0400

    feat: add more methods to vector

--- src/main/java/dev/akarah/cdata/script/value/mc/RVector.java
+++ src/main/java/dev/akarah/cdata/script/value/mc/RVector.java
@@ -1,8 +1,10 @@
 package dev.akarah.cdata.script.value.mc;
 
 import dev.akarah.cdata.script.expr.ast.func.MethodTypeHint;
+import dev.akarah.cdata.script.value.RNumber;
 import dev.akarah.cdata.script.value.RuntimeValue;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.phys.Vec3;
 
 public class RVector extends RuntimeValue {
@@ -29,6 +31,36 @@ public class RVector extends RuntimeValue {
     @MethodTypeHint(signature = "(lhs: vector, rhs: vector) -> vector", documentation = "Multiplies two vectors together element-wise, returning a new vector.")
     public static RVector multiply(RVector lhs, RVector rhs) {
         return RVector.of(lhs.javaValue().multiply(rhs.javaValue()));
+    }
+
+    @MethodTypeHint(signature = "(v: vector) -> number", documentation = "Gets the X component of this vector.")
+    public static RNumber x(RVector vector) {
+        return RNumber.of(vector.javaValue().x);
+    }
+
+    @MethodTypeHint(signature = "(v: vector) -> number", documentation = "Gets the Y component of this vector.")
+    public static RNumber y(RVector vector) {
+        return RNumber.of(vector.javaValue().y);
+    }
+
+    @MethodTypeHint(signature = "(v: vector) -> number", documentation = "Gets the Z component of this vector.")
+    public static RNumber z(RVector vector) {
+        return RNumber.of(vector.javaValue().z);
+    }
+
+    @MethodTypeHint(signature = "(vec: vector, value: number) -> vector", documentation = "Returns a new copy of this vector, with the new X component specified.")
+    public static RVector with_x(RVector vector, RNumber value) {
+        return RVector.of(vector.javaValue().with(Direction.Axis.X, value.doubleValue()));
+    }
+
+    @MethodTypeHint(signature = "(vec: vector, value: number) -> vector", documentation = "Returns a new copy of this vector, with the new Y component specified.")
+    public static RVector with_y(RVector vector, RNumber value) {
+        return RVector.of(vector.javaValue().with(Direction.Axis.Y, value.doubleValue()));
+    }
+
+    @MethodTypeHint(signature = "(vec: vector, value: number) -> vector", documentation = "Returns a new copy of this vector, with the new Z component specified.")
+    public static RVector with_z(RVector vector, RNumber value) {
+        return RVector.of(vector.javaValue().with(Direction.Axis.Z, value.doubleValue()));
     }
 
     public BlockPos asBlockPos() {
