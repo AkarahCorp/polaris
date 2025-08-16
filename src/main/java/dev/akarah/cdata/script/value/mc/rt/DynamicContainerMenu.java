f86f336a1f5f7237db75e85e8062df1e74a9b4f6
Whirling <infernity@infernity.dev>
2025-08-15 18:47:59 -0600

    fix: cursor items being voided

--- src/main/java/dev/akarah/cdata/script/value/mc/rt/DynamicContainerMenu.java
+++ src/main/java/dev/akarah/cdata/script/value/mc/rt/DynamicContainerMenu.java
@@ -60,5 +60,6 @@ public class DynamicContainerMenu extends ChestMenu {
                 "player.close_inventory",
                 REntity.of(player)
         );
+        super.removed(player);
     }
 }
