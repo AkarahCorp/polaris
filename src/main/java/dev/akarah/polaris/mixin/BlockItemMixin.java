e8d33abe7c178397968c55db2ec66415aac26ab9
akarahdev <endisticbutitsforgit@gmail.com>
2025-09-06 00:03:09 -0400

    feat: fix place block event location

--- src/main/java/dev/akarah/polaris/mixin/BlockItemMixin.java
+++ src/main/java/dev/akarah/polaris/mixin/BlockItemMixin.java
@@ -28,7 +28,7 @@ public class BlockItemMixin {
         var result = Resources.actionManager().performEvents(
                 "player.place_block",
                 REntity.of(blockPlaceContext.getPlayer()),
-                RVector.of(blockPlaceContext.getClickLocation())
+                RVector.of(blockPlaceContext.getClickLocation().add(blockPlaceContext.getClickedFace().getUnitVec3().multiply(0.5, 0.5, 0.5)))
         );
         if(!result) {
             var player = blockPlaceContext.getPlayer();
