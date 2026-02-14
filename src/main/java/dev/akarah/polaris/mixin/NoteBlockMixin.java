07b63cc5195093ffd050cfd6c354a68045b491c1
akarahdev <endisticbutitsforgit@gmail.com>
2026-02-14 10:52:50 -0500

    feat: noteblocks no longer update their instrument

---
+++ src/main/java/dev/akarah/polaris/mixin/NoteBlockMixin.java
@@ -0,0 +1,35 @@
+package dev.akarah.polaris.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.ScheduledTickAccess;
+import net.minecraft.world.level.block.NoteBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(NoteBlock.class)
+public class NoteBlockMixin {
+    @Inject(
+            method = "updateShape",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    public void stopShapeUpdating(
+            BlockState blockState,
+            LevelReader levelReader,
+            ScheduledTickAccess scheduledTickAccess,
+            BlockPos blockPos,
+            Direction direction,
+            BlockPos blockPos2,
+            BlockState blockState2,
+            RandomSource randomSource,
+            CallbackInfoReturnable<BlockState> cir
+    ) {
+        cir.cancel();
+    }
+}
